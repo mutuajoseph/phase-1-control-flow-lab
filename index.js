3053e8a982db098f2e93d9f367bca06e097a3f4f
mutuajoseph <wambuajoseph756@gmail.com>
2022-03-17 10:15:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let message;
+  if (feet <= 400){
+      message = "This one is on me!";
+  } else if (feet > 2000 && feet < 2500 ) {
+     message = "I will gladly take your thirty bucks.";
+  } else {
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result = (city === "NYC") ? "Ok, sounds good." : "No go.";
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  switch (tip) {
+    case "generous":
+      message = "Thank you so much.";
+      break;
+    case "not as generous":
+      message = "Thank you.";
+      break;
+    case "thanks for everything":
+      message = "Bye.";
+      break;
+
+  }
+  return message;
 }
